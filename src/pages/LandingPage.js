a696b9ee3f359b61867edfced5fc107e29d14e8c
enalmarzuki <40693134+enalmarzuki@users.noreply.github.com>
2020-07-12 14:54:50 +0800

    Fix Landing Page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,8 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimoni from "parts/Testimoni";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -21,6 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimoni data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
